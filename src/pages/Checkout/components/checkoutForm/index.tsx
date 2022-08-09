ca2368e72d246e5ed443aa1d0a402233af1c51fc
Gui Recordon <guilherme.recordon@gmail.com>
2022-08-09 17:04:34 -0300

    Finish bottom part of form checkout

--- src/pages/Checkout/components/checkoutForm/index.tsx
+++ src/pages/Checkout/components/checkoutForm/index.tsx
@@ -1,71 +1,90 @@
-import { MapPinLine } from 'phosphor-react'
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
 import {
   FormInput,
   LineOneInput,
   LineTwoInput,
   OptionalTag,
   StreetInput,
-  TopForm,
-  TopFormHeader,
+  FormSection,
+  FormHeader,
   ZipInput,
   FormContainer,
+  PaymentSelectionHolder,
+  PaymentButton,
 } from './styles'
 
 export function CheckoutForm() {
   return (
     <FormContainer>
-      <h3>Complete seu pedido</h3>
-      <TopForm>
-        <TopFormHeader>
-          <MapPinLine size={22} weight="regular" color="#C47F17" />
-          <div>
-            <h4>Endereço de Entrega</h4>
-            <p>Informe o endereço onde deseja receber seu pedido</p>
-          </div>
-        </TopFormHeader>
-        <ZipInput type="number" placeholder="CEP" />
-        <StreetInput type="text" placeholder="Rua" />
-        <LineOneInput>
-          <FormInput type="number" placeholder="Número" />
-          <FormInput type="text" placeholder="Complemento" />
-          <OptionalTag>Opcional</OptionalTag>
-        </LineOneInput>
-        <LineTwoInput>
-          <FormInput type="text" placeholder="Bairro" />
-          <FormInput type="text" placeholder="Cidade" />
-          <FormInput type="text" placeholder="UF" />
-        </LineTwoInput>
-      </TopForm>
       <div>
-        <header>
-          <div>Dollar Sign</div>
-          <h4>Pagamento</h4>
-          <p>
-            O pagamento é feito na entrega. Escolha a forma que deseja pagar
-          </p>
-        </header>
+        <h3>Complete seu pedido</h3>
+        <FormSection>
+          <FormHeader>
+            <MapPinLine size={22} weight="regular" color="#C47F17" />
+            <div>
+              <h4>Endereço de Entrega</h4>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </FormHeader>
+          <ZipInput type="number" placeholder="CEP" />
+          <StreetInput type="text" placeholder="Rua" />
+          <LineOneInput>
+            <FormInput type="number" placeholder="Número" />
+            <FormInput type="text" placeholder="Complemento" />
+            <OptionalTag>Opcional</OptionalTag>
+          </LineOneInput>
+          <LineTwoInput>
+            <FormInput type="text" placeholder="Bairro" />
+            <FormInput type="text" placeholder="Cidade" />
+            <FormInput type="text" placeholder="UF" />
+          </LineTwoInput>
+        </FormSection>
+        <FormSection>
+          <FormHeader>
+            <CurrencyDollar size={22} weight="regular" color="#8047F8" />
+
+            <div>
+              <h4>Pagamento</h4>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </FormHeader>
+          <PaymentSelectionHolder>
+            <PaymentButton>
+              <input type="radio" value="credit" />
+              <CreditCard size={16} color="#8047F8" />
+              <label htmlFor="credit">Cartão de Crédito</label>
+            </PaymentButton>
+            <PaymentButton>
+              <input type="radio" value="debit" />
+              <Bank size={16} color="#8047F8" />
+              <label htmlFor="debit">Cartão de Débito</label>
+            </PaymentButton>{' '}
+            <PaymentButton>
+              <input type="radio" value="cash" />
+              <Money size={16} color="#8047F8" />
+
+              <label htmlFor="cash">Dinheiro</label>
+            </PaymentButton>
+          </PaymentSelectionHolder>
+        </FormSection>
+      </div>
+      <div>
         <div>
-          <div>
-            <input type="radio" value="credit" />
-            <label htmlFor="credit">Cartão de Crédito</label>
-          </div>
-          <div>
-            <input type="radio" value="debit" />
-            <label htmlFor="debit">Cartão de Débito</label>
-          </div>{' '}
-          <div>
-            <input type="radio" value="cash" />
-            <label htmlFor="cash">Dinheiro</label>
-          </div>
+          <h4>Cafés selecionados</h4>
+          <div>Confirmation card</div>
+          <div>Confirmation card</div>
+          <div>Price Confirmation</div>
         </div>
+        <button>Confirmar pedido</button>
       </div>
-      <div>
-        <h4>Cafés selecionados</h4>
-        <div>Confirmation card</div>
-        <div>Confirmation card</div>
-        <div>Price Confirmation</div>
-      </div>
-      <button>Confirmar pedido</button>
     </FormContainer>
   )
 }
--- src/pages/Checkout/components/checkoutForm/styles.ts
+++ src/pages/Checkout/components/checkoutForm/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
 export const FormContainer = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 448px;
+  gap: 2rem;
+
   h3 {
     font-size: 1.125rem;
     font-family: 'Baloo 2', cursive;
@@ -9,13 +13,14 @@ export const FormContainer = styled.form`
   }
 `
 
-export const TopForm = styled.div`
+export const FormSection = styled.div`
   background: ${(props) => props.theme['base-card']};
   padding: 2.5rem;
   border-radius: 6px;
+  margin-bottom: 0.75rem;
 `
 
-export const TopFormHeader = styled.header`
+export const FormHeader = styled.header`
   display: flex;
   gap: 0.5rem;
 
@@ -78,4 +83,39 @@ export const OptionalTag = styled.span`
   position: absolute;
   right: 1rem;
   top: 14px;
+`
+
+export const PaymentSelectionHolder = styled.div`
+  padding: 1rem 0;
+  display: flex;
+  gap: 0.75rem;
+`
+export const PaymentButton = styled.div`
+  flex: 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
+
+  padding: 1rem;
+  background: ${(props) => props.theme['base-button']};
+  border-radius: 6px;
+  color: ${(props) => props.theme['base-text']};
+  font-size: 0.75rem;
+  font-weight: 400;
+  text-transform: uppercase;
+
+  input {
+    left: -999rem;
+    position: absolute;
+  }
+
+  :hover {
+    background: ${(props) => props.theme['base-hover']};
+  }
+
+  :focus {
+    border: solid 1px ${(props) => props.theme.purple};
+  }
 `
