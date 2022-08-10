60dbc4825d5b6d5f8a1143aa4220e5a6893753d3
Gui Recordon <guilherme.recordon@gmail.com>
2022-08-10 15:35:26 -0300

    Finish styling success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,8 +1,15 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 import {
+  BulletWrap,
+  DollarPinWrapper,
   FakeGradientBorder,
+  MapPinWrapper,
   SuccessContainer,
   SuccessLeftSide,
+  SuccessRightSide,
   SummaryPurchase,
+  TimerPinWrapper,
+  WrapOfAllBullets,
 } from './styles'
 
 export function Success() {
@@ -13,37 +20,47 @@ export function Success() {
         <h4>Agora é só aguardar que logo o café chegará até você</h4>
         <SummaryPurchase>
           <FakeGradientBorder>
-            <div>
-              <span>Icon</span>
-              <div>
-                <p>Previsão de entrega</p>
-                <p>
-                  <strong>20 min - 30 min</strong>
-                </p>
-              </div>
-            </div>
-            <div>
-              <span>Icon</span>
-              <div>
-                <p>
-                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
-                </p>
-                <p>Farrapos - Porto Alegre, RS</p>
-              </div>
-            </div>
-            <div>
-              <span>Icon</span>
-              <div>
-                <p>Pagamento na entrega</p>
-                <p>
-                  <strong>Cartão de Crédito</strong>
-                </p>
-              </div>
-            </div>
+            <WrapOfAllBullets>
+              <BulletWrap>
+                <MapPinWrapper>
+                  <MapPin size={16} color="#fff" weight="fill" />
+                </MapPinWrapper>
+                <div>
+                  <p>Previsão de entrega</p>
+                  <p>
+                    <strong>20 min - 30 min</strong>
+                  </p>
+                </div>
+              </BulletWrap>
+              <BulletWrap>
+                <TimerPinWrapper>
+                  <Timer size={16} color="#fff" weight="fill" />
+                </TimerPinWrapper>
+                <div>
+                  <p>
+                    Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                  </p>
+                  <p>Farrapos - Porto Alegre, RS</p>
+                </div>
+              </BulletWrap>
+              <BulletWrap>
+                <DollarPinWrapper>
+                  <CurrencyDollar size={16} color="#fff" weight="fill" />
+                </DollarPinWrapper>
+                <div>
+                  <p>Pagamento na entrega</p>
+                  <p>
+                    <strong>Cartão de Crédito</strong>
+                  </p>
+                </div>
+              </BulletWrap>
+            </WrapOfAllBullets>
           </FakeGradientBorder>
         </SummaryPurchase>
       </SuccessLeftSide>
-      <img src="src/assets/Illustration.svg" alt="" />
+      <SuccessRightSide>
+        <img src="src/assets/Illustration.svg" alt="" />
+      </SuccessRightSide>
     </SuccessContainer>
   )
 }
